a56bb0b1965f7a7fbd31aa89af7e71a46b0a9475
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2024-05-26 02:36:08 -0300

    Background de Login agregado

--- login/index.php
+++ login/index.php
@@ -20,7 +20,16 @@ include('../app/config.php');
     <link rel="stylesheet" href="<?= APP_URL; ?>/public/dist/css/adminlte.min.css">
 </head>
 
-<body class="hold-transition login-page">
+<style>
+    .background-image {
+    background-image: url('https://img.freepik.com/vector-gratis/diseno-fondo-abstracto-blanco_23-2148825582.jpg?t=st=1716700950~exp=1716704550~hmac=3dbbdf12f9260e273185f1224a8183de89f6ce29773da4d170e01f3d39d8f551&w=1380');
+    background-size: cover;
+    background-position: center;
+}
+</style>
+
+<body class="hold-transition login-page background-image">
+    
     <div class="login-box">
         <center><img src="<?= APP_URL; ?>/public/images/capdevila.png" width="200px" alt="">
             <br>
