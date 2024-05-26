8bd6b8962eea8648bc6cbcf957c032e8e1277321
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2024-05-25 21:45:37 -0300

    Creación de interface

---
+++ app/config.php
@@ -0,0 +1,30 @@
+<?php   
+
+define('SERVIDOR','localhost');
+define('USUARIO','root');
+define('PASSWORD','');
+define('BD','aulaunida');
+
+define('APP_NAME','SISTEMA DE GESTIÓN ESCOLAR');
+define('APP_URL','http://localhost/aulaunida');
+define('KEY_API_MAPS','');
+
+$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
+
+try{
+    $pdo = new PDO($servidor,USUARIO,PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+    //echo "conexión existosa a la base de datos";
+}catch (PDOException $e){
+    print_r($e);
+    echo "error no se pudo conectar a la base de datos";
+}
+
+date_default_timezone_set("America/Argentina/Cordoba");
+$fechaHora = date('Y-m-d H:i:s');
+
+$fecha_actual = date('Y-m-d');
+$dia_actual = date('d');
+$mes_actual = date('m');
+$ano_actual = date('Y');
+
+
